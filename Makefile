e7c7929763f012b9de04117864ac14cc54c5822d
insight <insight3773@ya.ru>
2021-08-12 21:23:12 +0300

    Lesson 3. Add telegram SDK, webhook. Work In Progress

--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@ install:
 	touch .env.local
 
 run-server:
-	php -S localhost:8080 -t public/
+	php -S localhost:8081 -t public/
--- src/Controller/TelegramController.php
+++ src/Controller/TelegramController.php
@@ -3,8 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use TgBotApi\BotApiBase\BotApiInterface;
+use TgBotApi\BotApiBase\Method\SendMessageMethod;
+use TgBotApi\BotApiBase\WebhookFetcherInterface;
 
 class TelegramController extends AbstractController
 {
+    /**
+     * @Route("/webhook", name="telegram_webhook")
+     */
+    public function webhook(Request $request, WebhookFetcherInterface $webhookFetcher, BotApiInterface $botApi): JsonResponse
+    {
+        $update = $webhookFetcher->fetch($request);
 
+
+        $method = SendMessageMethod::create($update->message->chat->id, 'TEST');
+        $botApi->send($method);
+        dd($update);
+    }
 }
